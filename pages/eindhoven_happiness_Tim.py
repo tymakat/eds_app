2ff5165c4cb37ca7180fd103ca32cae80e1e8234
Tymofii Katyshev <tymakat@gmail.com>
2024-01-20 22:27:46 +0100

    Update eindhoven_happiness_Tim.py

--- pages/eindhoven_happiness_Tim.py
+++ pages/eindhoven_happiness_Tim.py
@@ -25,7 +25,8 @@ def create_map(df):
     (0, "grey"),  # Color for 0 values
     # Define other colors for the rest of your scale
     (0.7, "red"),  # Example: Color for values > 0
-    (1, "white")   # Ensure the scale covers the full range of your data
+    (0.9, "white") 
+    (1, "green") # Ensure the scale covers the full range of your data
     ]
     fig = px.choropleth_mapbox(
         df,
