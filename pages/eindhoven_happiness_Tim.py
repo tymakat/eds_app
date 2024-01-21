dda23b51f9a44ecbc0f20dfbe7d790d2c75743c6
Tymofii Katyshev <tymakat@gmail.com>
2024-01-21 02:06:23 +0100

    Update eindhoven_happiness_Tim.py

--- pages/eindhoven_happiness_Tim.py
+++ pages/eindhoven_happiness_Tim.py
@@ -129,7 +129,7 @@ def app():
 
     # Calculate the average age for each group
     grouped_avg_age = smileys_replaced.groupby('Group')['average age'].mean().reindex(labels)
-    fig3, ax = plt.subplots(figsize=(8, 7))
+    fig3, ax = plt.subplots(figsize=(10, 7))
     colors = ['#46874b', '#76b37a', '#a9ccac', '#dea0a0', '#d46868', '#9c3333']
 
     grouped_avg_age.plot(kind='bar', ax=ax, color=colors)
