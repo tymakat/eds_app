41f0d8ae337150bcfb8af8c0ca9b27916d052a89
Tymofii Katyshev <tymakat@gmail.com>
2024-01-21 01:52:06 +0100

    Update eindhoven_happiness_Tim.py

--- pages/eindhoven_happiness_Tim.py
+++ pages/eindhoven_happiness_Tim.py
@@ -144,7 +144,7 @@ def app():
                 ha='center', va='center', xytext=(0, 10), textcoords='offset points')
 
     # Adjust the y-axis range
-    ax.set_ylim(25, 50)
+    ax.set_ylim(25, 45)
 
     # Show the plot in Streamlit
     buf = BytesIO()
